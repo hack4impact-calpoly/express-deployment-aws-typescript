029df0faa355abc39d22023354561984a23fd98c
Ethan Zimbelman <itszimbo@gmail.com>
2022-05-01 09:29:43 -0700

    typescript modifications
    followed instructions from <https://webpack.js.org/guides/typescript/>

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,24 @@
 const path = require('path');
 const Dotenv = require('dotenv-webpack');
 
+// https://webpack.js.org/guides/typescript/
 module.exports = {
   entry: './lambda.js',
   target: 'node',
   mode: 'production',
   devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
