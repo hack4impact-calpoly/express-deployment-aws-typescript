130a8c789f8e758757b0846cabdb70cb811b27ab
Ethan Zimbelman <itszimbo@gmail.com>
2022-05-04 19:13:27 -0700

    log Express app object

--- lambda.js
+++ lambda.js
@@ -1,6 +1,7 @@
 require('source-map-support/register');
 const serverlessExpress = require('@vendia/serverless-express');
 const app = require('./backend/server.ts');
+console.log(app);
 console.log(app.handle);
 console.log("**typeof app.handle:::"+typeof(app.handle));
 exports.handler = serverlessExpress({ app });
