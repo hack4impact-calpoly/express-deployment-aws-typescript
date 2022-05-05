01d11cbc818cf1acc5aa9614cc79ef2d61bfdcb8
Ethan Zimbelman <itszimbo@gmail.com>
2022-05-04 19:19:46 -0700

    require default export from server.ts

--- lambda.js
+++ lambda.js
@@ -1,6 +1,6 @@
 require('source-map-support/register');
 const serverlessExpress = require('@vendia/serverless-express');
-const app = require('./backend/server.ts');
+const app = require('./backend/server.ts').default;
 console.log(app);
 console.log(app.handle);
 console.log("**typeof app.handle:::"+typeof(app.handle));
