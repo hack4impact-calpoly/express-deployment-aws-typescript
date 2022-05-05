5228864fe965fc550be4b44d3ba8c32cc8b23d50
Ethan Zimbelman <itszimbo@gmail.com>
2022-05-05 10:24:52 -0700

    remove logging statements

--- lambda.js
+++ lambda.js
@@ -1,7 +1,4 @@
 require('source-map-support/register');
 const serverlessExpress = require('@vendia/serverless-express');
 const app = require('./backend/server.ts').default;
-console.log(app);
-console.log(app.handle);
-console.log("**typeof app.handle:::"+typeof(app.handle));
 exports.handler = serverlessExpress({ app });
